799c2446731c37cdbe3d76f74f19de3cc3e5f0e2
cygwong <gwong98@gmail.com>
2019-09-15 23:19:36 -0700

    one loop std

--- task2/task2.c
+++ task2/task2.c
@@ -43,6 +43,25 @@ void find_mean_std(float*data, int len, float* out_mean, float*out_std)
 	*out_mean = find_mean(data,len);
 	*out_std = find_std(data,len);
 }
+
+void find_mean_std2(float*data, int len, float* out_mean, float*out_std)
+{
+	float sum = 0;
+	float sq_sum = 0;
+
+	for (int i = 0; i < len; ++i)
+	{
+		sum += data[i];
+		sq_sum += data[i]*data[i];
+	}
+
+	float ave = sum/len;
+	float std_value = sq_sum/len - ave * ave;
+	std_value = sqrt(std_value);
+
+	*out_mean = ave;
+	*out_std = std_value;
+}
 int main()
 {
 	float data [] ={0.068038, 0.173967, 0.439303, 0.615258, 0.789436, 0.770980, 0.890924, 1.034431, 0.955555, 0.991579, 0.919359, 0.857244, 0.680064, 0.558250, 0.473129, 0.278329, 0.027142, -0.151631, -0.454363, -0.534177, -0.803847, -0.882892, -0.996433, -0.919950, -1.068664, -1.000596, -0.997921, -0.909708, -0.607322, -0.611919, -0.380097, -0.127268};
@@ -68,7 +87,7 @@ int main()
 	float mean1;
 	float std1;
 
-	find_mean_std(data,length,&mean1,&std1);
+	find_mean_std2(data,length,&mean1,&std1);
 
 	printf("\nMean = %f", mean1);
 	printf("\nStandard deviation = %f", std1);
