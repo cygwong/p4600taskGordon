322a23a76cc24629e5d31572c18c0360f6168976
U-PHYSICSR302\Advanced Student <Advanced Student@PhysicsR302.kwantlen.ca>
2019-10-03 11:50:19 -0700

    connection to VISA still need to be fixed

---
+++ task5/src/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>	
+#include <visa.h>
+
+void main(int argc, char** argv)
+{
+	char inq[] = "*IDN?/n";
+	ViStatus status = VI_SUCCESS;
+	ViFindList resourceList;
+	ViUInt32 num_inst;
+
+	ViSession defaultRM, scopeHandle;
+	ViChar description[VI_FIND_BUFLEN];
+
+	status = viOpenDefaultRM(&defaultRM);
+
+	if(status == VI_SUCCESS)
+	{
+		status = viFindRsrc(defaultRM,"USB[0-9]::0?*INSTR",
+						&resourceList,&num_inst,description);
+		if(status == VI_SUCCESS)
+		{
+			status = viOpen(defaultRM,description,
+							VI_NULL,VI_NULL,&scopeHandle);
+
+			if(status == VI_SUCCESS)
+			{
+				printf("\nOpened %s",description);
+			}
+			else
+			{
+				printf("\nFailed to open %s",description);
+			}
+		}
+		else
+		{
+			printf("\nCouldn't find any instruments");
+		}
+	}
+	else{
+			printf("Failed to open defaultRM");
+		}
+}
