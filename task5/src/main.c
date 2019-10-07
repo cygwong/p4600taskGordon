2139969e5c376832dd6715fc0ee4843fcc16f9a2
cygwong <gwong98@gmail.com>
2019-10-07 12:21:07 -0700

    communication established and works on laptop

--- task5/src/main.c
+++ task5/src/main.c
@@ -1,44 +1,64 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>	
+#include <stdio.h>
+#include <math.h>
 #include <visa.h>
-
-void main(int argc, char** argv)
-{
+//#include "../include/visa.h"
+int main(){
+	unsigned char resultBuffer[256];
 	char inq[] = "*IDN?/n";
 	ViStatus status = VI_SUCCESS;
 	ViFindList resourceList;
 	ViUInt32 num_inst;
+	ViUInt32 resultCount;
 
 	ViSession defaultRM, scopeHandle;
 	ViChar description[VI_FIND_BUFLEN];
+	char dataBuffer[2500];
+
+	int y;
+	int lsb;
+	int masb;
 
 	status = viOpenDefaultRM(&defaultRM);
+	printf("I am working\n");
 
 	if(status == VI_SUCCESS)
 	{
-		status = viFindRsrc(defaultRM,"USB[0-9]::0?*INSTR",
-						&resourceList,&num_inst,description);
+		printf("I am working1\n");
+		status = viFindRsrc(defaultRM,"USB[0-9]::?*INSTR",&resourceList,&num_inst,description);
+		printf("I am working2\n");
 		if(status == VI_SUCCESS)
 		{
-			status = viOpen(defaultRM,description,
-							VI_NULL,VI_NULL,&scopeHandle);
-
+			printf("I am working3\n");
+			status = viOpen(defaultRM,description,VI_NULL,VI_NULL,&scopeHandle);
+			printf("I am working4\n");
 			if(status == VI_SUCCESS)
 			{
-				printf("\nOpened %s",description);
+				printf("I am working5\n");
+				printf("Opened instriment %s\n",description);
+
+				viWrite(scopeHandle,"*IDN?\n",6,&resultCount);
+				viRead(scopeHandle,resultBuffer,256,&resultCount);
+
+				printf("\nResult count: %d\n",resultCount);
+				printf("\nResult buffer: %s\n",resultBuffer);
+
+				viWrite(scopeHandle,"CURV?\n",6,&resultCount);
+				viRead(scopeHandle,dataBuffer,2500,&resultCount);
+
+				for(int i = 0; i <128; i++)
+				{
+					y = dataBuffer[i];
+					printf("\nRaw = %x, Read = %d",y,y);
+				}
 			}
-			else
-			{
-				printf("\nFailed to open %s",description);
+			else{
+				printf("Faild to open %s\n",description );
 			}
 		}
-		else
-		{
-			printf("\nCouldn't find any instruments");
+		else{
+			printf("Failed to find connection \n");
 		}
 	}
-	else{
-			printf("Failed to open defaultRM");
-		}
+	return 0;
 }
